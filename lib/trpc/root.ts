58a702a04815ac4be160d90b1ab900440486a88d
claudio <claudioorjunior@gmail.com>
2026-07-23 01:57:50 -0300

    feat: add AGA router, registros router, sinaisVitais router, timeline, escala interpretations

--- lib/trpc/root.ts
+++ lib/trpc/root.ts
@@ -2,11 +2,17 @@ import { createTRPCRouter } from './server';
 import { pacientesRouter } from './routers/pacientes';
 import { instituicoesRouter } from './routers/instituicoes';
 import { usuariosRouter } from './routers/usuarios';
+import { avaliacoesGeriatricasRouter } from './routers/avaliacoesGeriatricas';
+import { registrosRouter } from './routers/registros';
+import { sinaisVitaisRouter } from './routers/sinaisVitais';
 
 export const appRouter = createTRPCRouter({
   pacientes: pacientesRouter,
   instituicoes: instituicoesRouter,
   usuarios: usuariosRouter,
+  avaliacoesGeriatricas: avaliacoesGeriatricasRouter,
+  registros: registrosRouter,
+  sinaisVitais: sinaisVitaisRouter,
 });
 
 export type AppRouter = typeof appRouter;
